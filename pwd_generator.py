b1174b33f1322e49bc116ee14f2405458f92919e
Andrew Nelson <26756779+dnelso1@users.noreply.github.com>
2022-08-08 23:29:34 -0400

    Finished project for final submission

--- pwd_generator.py
+++ pwd_generator.py
@@ -1,26 +1,16 @@
 import random, pyperclip, pika
 import tkinter as tk
-import tkinter.ttk
 from PIL import Image, ImageTk
 
 LOWER = "abcdefghijklmnopqrstuvwxyz"
 UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-NUMBERS = "0123456789"
-SPECIALS = "!@#$%^&*()}{?"
+NUMBERS = "01234567890123456789"
+SPECIALS = "!@#$%^&*()}{?!@#$%^&*()}{?"
 
-# create GUI and variables
+# create GUI
 root = tk.Tk()
-root.geometry("428x500")
+root.geometry("427x500")
 root.title("Password Generator")
-pwd_length = tk.IntVar()
-low_letters = tk.IntVar()
-cap_letters = tk.IntVar()
-nums = tk.IntVar()
-symbols = tk.IntVar()
-nums_min = tk.IntVar()
-symbols_min = tk.IntVar()
-spinbox_max = tk.StringVar()
-
 
 
 # * * * * * * * * * * * * * * * *
@@ -64,8 +54,9 @@ def copyPwd(src):
         pyperclip.copy(pwd)
 
 def lenSlide(var):
-    slide_label = tk.Label(root, text=len_slider.get())
-    slide_label.grid(column=1, row=20, sticky="W", ipadx=52)
+    # displays the length slider value as it changes
+    slide_label = tk.Label(pwd_frame, text=len_slider.get())
+    slide_label.grid(column=1, row=2, sticky="W", ipadx=52)
 
 def convertToL33t():
     # create the RabbitMQ connection and channel
@@ -92,137 +83,128 @@ def convertToL33t():
     channel.basic_consume(queue='converted', on_message_callback=on_response, auto_ack=True)
     channel.start_consuming()
 
-"""def displayConvertedPwd(pwd):
-    l33t_entry.delete(0, tk.END)
-    l33t_entry.insert(10, pwd)"""
-
-"""def numSlide(var):
-    slide_label = tk.Label(root, text=nums_slider.get())
-    slide_label.grid(column=1, row=50, sticky="W", ipadx=52)
-    slider = tk.Scale(root, variable=nums_min, from_=2, to=pwd_length.get(), orient="horizontal", showvalue=0, command=numSlide)
-    slider.grid(column=1, row=50, sticky="E")
-
-def symbolSlide(var):
-    slide_label = tk.Label(root, text=symbols_slider.get())
-    slide_label.grid(column=1, row=55, sticky="W", ipadx=52)"""
-
-"""def spinbox(var):
-    # spin box for minimum numbers
-    nums_spinbox = tk.Spinbox(root, from_=2, to=len_slider.get(), width=2)
-    nums_spinbox.grid(column=1, row=50, sticky="E")
-
-    # spin box for minimum special characters
-    symbols_spinbox = tk.Spinbox(root, from_=2, to=len_slider.get(), width=2)
-    symbols_spinbox.grid(column=1, row=55, sticky="E")"""
-
-
 
 # * * * * * * * * * * * * * * * *
-#  Labels
+#  Password Frame Labels
 # * * * * * * * * * * * * * * * *
 
-# label for password
-pwd_entry = tk.Entry(root, width=26)
-pwd_entry.grid(column=1, row=5, sticky="W")
+pwd_frame = tk.Frame(root)
+pwd_frame.grid(column=0, row=0)
 
 # label for pwd length
-length_label = tk.Label(root, text="Length")
-length_label.grid(column=0, row=20, sticky="SW")
+length_label = tk.Label(pwd_frame, text="Length")
+length_label.grid(column=0, row=2, sticky="SW")
+
+# label for options
+options_label = tk.Label(pwd_frame, text="Choose options for a secure password:")
+options_label.grid(column=0, row=1, columnspan=2, pady=10, sticky="SW")
 
 # label for lowercase letters
-lows_label = tk.Label(root, text="Lowercase")
-lows_label.grid(column=0, row=25, sticky="W")
-lows_hover = tk.Label(root, text="ex: password", font=('*Font', '8'), fg="gray")
-lows_hover.grid(column=1, row=25, sticky="W")
+lows_label = tk.Label(pwd_frame, text="Lowercase")
+lows_label.grid(column=0, row=3, sticky="W")
+lows_hover = tk.Label(pwd_frame, text="ex: password", font=('*Font', '8'), fg="gray")
+lows_hover.grid(column=1, row=3, sticky="W")
 
 # label for capital letters
-caps_label = tk.Label(root, text="Capitals")
-caps_label.grid(column=0, row=30, sticky="W")
-caps_hover = tk.Label(root, text="ex: pAsswOrD", font=('*Font', '8'), fg="gray")
-caps_hover.grid(column=1, row=30, sticky="W")
-#caps_hover = tk.Label(root, text="", width=40)
-#caps_enter = caps_hover.configure(text="Include capital letters in your password.")
-#caps_leave = caps_hover.configure(text="")
-#caps_label.bind("<Enter>", caps_enter)
-#caps_label.bind("<Leave>", caps_leave)
+caps_label = tk.Label(pwd_frame, text="Capitals")
+caps_label.grid(column=0, row=4, sticky="W")
+caps_hover = tk.Label(pwd_frame, text="ex: pAsswOrD", font=('*Font', '8'), fg="gray")
+caps_hover.grid(column=1, row=4, sticky="W")
 
 # label for numbers
-nums_label = tk.Label(root, text="Numbers")
-nums_label.grid(column=0, row=35, sticky="W")
-nums_hover = tk.Label(root, text="ex: p4ssw9r2", font=('*Font', '8'), fg="gray")
-nums_hover.grid(column=1, row=35, sticky="W")
+nums_label = tk.Label(pwd_frame, text="Numbers")
+nums_label.grid(column=0, row=5, sticky="W")
+nums_hover = tk.Label(pwd_frame, text="ex: p4ssw9r2", font=('*Font', '8'), fg="gray")
+nums_hover.grid(column=1, row=5, sticky="W")
 
 # label for special characters (symbols)
-symbols_label = tk.Label(root, text="Symbols")
-symbols_label.grid(column=0, row=40, sticky="W")
-symbols_hover = tk.Label(root, text="ex: p&ssw!r#", font=('*Font', '8'), fg="gray")
-symbols_hover.grid(column=1, row=40, sticky="W")
-
-# label for L33T conversion
-l33t_label = tk.Label(root, text="L33T Converter:")
-l33t_label.grid(column=0, row=45, columnspan=2, sticky="W")
-l33t_entry = tk.Entry(root, width=26)
-l33t_entry.grid(column=1, row=50, sticky="W")
+symbols_label = tk.Label(pwd_frame, text="Symbols")
+symbols_label.grid(column=0, row=6, sticky="W")
+symbols_hover = tk.Label(pwd_frame, text="ex: p&ssw!r#", font=('*Font', '8'), fg="gray")
+symbols_hover.grid(column=1, row=6, sticky="W")
 
 
+# * * * * * * * * * * * * * * * * * * *
+#  Password Frame Interactive Elements
+# * * * * * * * * * * * * * * * * * * *
 
-# * * * * * * * * * * * * * * * *
-#  Interactive Elements
-# * * * * * * * * * * * * * * * *
+# entry box for password
+pwd_entry = tk.Entry(pwd_frame, width=32)
+pwd_entry.grid(column=1, row=0, sticky="W")
 
-# combo box for length values
-"""combo = tk.ttk.Combobox(root, textvariable=pwd_length)
-combo['values'] = (8, 9, 10, 11, 12, 13, 14, 15, 16,
-                   17, 18, 19, 20, 21, 22, 23, 24, 25,
-                   26, 27, 28, 29, 30, 31, 32)
-combo.current(8)
-combo.bind('<<ComboboxSelected>>')
-combo.grid(column=1, row=1)"""
-
-# regenerate button
+# password regenerate and copy buttons
 original_regen_img = Image.open("Images/regenerate.jpeg")
 resized_regen_img = original_regen_img.resize((20, 20), Image.Resampling.LANCZOS)
 regen_img = ImageTk.PhotoImage(resized_regen_img)
-gen_button = tk.Button(root, image=regen_img, command=generatePwd)
-gen_button.grid(column=2, row=5)
+regen_button = tk.Button(pwd_frame, image=regen_img, command=generatePwd)
+regen_button.grid(column=2, row=0)
 
-# password copy button
 original_copy_img = Image.open("Images/copy.jpeg")
 resized_copy_img = original_copy_img.resize((20, 20), Image.Resampling.LANCZOS)
 copy_img = ImageTk.PhotoImage(resized_copy_img)
-copy_button = tk.Button(root, image=copy_img, command=lambda: copyPwd(''))
-copy_button.grid(column=3, row=5)
+copy_button = tk.Button(pwd_frame, image=copy_img, command=lambda: copyPwd(''))
+copy_button.grid(column=3, row=0)
 
 # slider for length values
-len_slider = tk.Scale(root, variable=pwd_length, from_=8, to=32, orient="horizontal", showvalue=0, command=lenSlide)
+pwd_length = tk.IntVar()
+len_slider = tk.Scale(pwd_frame, variable=pwd_length, from_=8, to=32, orient="horizontal", showvalue=0, command=lenSlide)
 len_slider.set(16)
-len_slider.grid(column=1, row=20, sticky="E")
+len_slider.grid(column=1, row=2, sticky="E")
 
-# check box for lowercase letters
-low_box = tk.Checkbutton(root, variable=low_letters, onvalue=1, offvalue=0, command=generatePwd)
-low_box.grid(column=1, row=25, sticky="E")
+# checkboxes for lowercase/uppercase letters, numbers, and special characters
+low_letters = tk.IntVar()
+low_box = tk.Checkbutton(pwd_frame, variable=low_letters, onvalue=1, offvalue=0, command=generatePwd)
+low_box.grid(column=1, row=3, sticky="E")
 low_box.select()
 
+cap_letters = tk.IntVar()
+caps_box = tk.Checkbutton(pwd_frame, variable=cap_letters, onvalue=1, offvalue=0, command=generatePwd)
+caps_box.grid(column=1, row=4, sticky="E")
+caps_box.select()
+
+nums = tk.IntVar()
+nums_box = tk.Checkbutton(pwd_frame, variable=nums, onvalue=1, offvalue=0, command=generatePwd)
+nums_box.grid(column=1, row=5, sticky="E")
+nums_box.select()
+
+symbols = tk.IntVar()
+symbols_box = tk.Checkbutton(pwd_frame, variable=symbols, onvalue=1, offvalue=0, command=generatePwd)
+symbols_box.grid(column=1, row=6, sticky="E")
+
+
+# * * * * * * * * * * * * * * * *
+#  L33t Frame Labels
+# * * * * * * * * * * * * * * * *
+
+l33t_frame = tk.Frame(root)
+l33t_frame.grid(column=0, row=2, pady=50)
+
+# labels for l33T conversion
+l33t_label = tk.Label(l33t_frame, text="L33t Converter:")
+l33t_label.grid(column=0, row=0, columnspan=2, sticky="W")
+l33t_description = tk.Label(l33t_frame, text="Convert your weak passwords to strong ones", font=('*Font', '8', "italic"), fg="gray")
+l33t_description.grid(column=0, row=3, columnspan=2, sticky="W")
+
+
+# * * * * * * * * * * * * * * * * *
+#  L33t Frame Interactive Elements
+# * * * * * * * * * * * * * * * * *
+
+# entry box for l33t conversion
+l33t_entry = tk.Entry(l33t_frame, width=26)
+l33t_entry.grid(column=1, row=1, sticky="W")
+
+# l33t conversion and copy buttons
+original_convert_img = Image.open("Images/convert.jpeg")
+resized_convert_img = original_convert_img.resize((20, 20), Image.Resampling.LANCZOS)
+convert_img = ImageTk.PhotoImage(resized_convert_img)
+l33t_button = tk.Button(l33t_frame, image=convert_img, command=convertToL33t)
+l33t_button.grid(column=2, row=1)
+
+l33t_copy_button = tk.Button(l33t_frame, image=copy_img, command=lambda: copyPwd('convert'))
+l33t_copy_button.grid(column=3, row=1)
+
+# generate password upon program start
 generatePwd()
-
-# check box for capital letters
-caps_box = tk.Checkbutton(root, variable=cap_letters, onvalue=1, offvalue=0, command=generatePwd)
-caps_box.grid(column=1, row=30, sticky="E")
-
-# check box for numbers
-nums_box = tk.Checkbutton(root, variable=nums, onvalue=1, offvalue=0, command=generatePwd)
-nums_box.grid(column=1, row=35, sticky="E")
-
-# check box for special characters (symbols)
-symbols_box = tk.Checkbutton(root, variable=symbols, onvalue=1, offvalue=0, command=generatePwd)
-symbols_box.grid(column=1, row=40, sticky="E")
-
-# l33t conversion button
-l33t_button = tk.Button(root, text="Convert", command=convertToL33t)
-l33t_button.grid(column=2, row=50)
-
-# l33t copy button
-l33t_copy_button = tk.Button(root, image=copy_img, command=lambda: copyPwd('convert'))
-l33t_copy_button.grid(column=3, row=50)
 
 root.mainloop()
